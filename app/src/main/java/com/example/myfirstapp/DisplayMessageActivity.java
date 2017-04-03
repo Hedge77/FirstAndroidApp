31b9da17114ca3b05c78996d33afb22a2e3a4354
Ryan Roe <ryan.r.roe@hotmail.com>
2017-04-03 01:29:24 -0700

    added messages

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -17,5 +18,42 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textView2);
         textView.setText(message);
+
+        Log.v("DisplayMessageActivity", "OnCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("DisplayMessageActivity", "OnStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("DisplayMessageActivity", "OnResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.w("DisplayMessageActivity", "OnPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.e("DisplayMessageActivity", "OnStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.e("DisplayMessageActivity", "OnDestroy()");
     }
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("MainActivity", "OnCreate()");
     }
 
     public void sendMessage(View view) {
@@ -22,5 +24,40 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MainActivity", "OnStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("MainActivity", "OnResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.w("MainActivity", "OnPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.e("MainActivity", "OnStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.e("MainActivity", "OnDestroy()");
     }
 }
